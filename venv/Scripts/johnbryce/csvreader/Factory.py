e8dda79c89cd53c74baeb6039d138cf277e27c56
gilads <sargilad@gmail.com>
2021-06-02 18:07:46 +0300

    added validations to Factory.py

--- venv/Scripts/johnbryce/csvreader/Factory.py
+++ venv/Scripts/johnbryce/csvreader/Factory.py
@@ -12,22 +12,24 @@ class Factory:
         self.name = name
 
     def get_id_by_name(self):
-        with open(CUST_CSV) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=",")
-            for row in csv_reader:
-                if row[1] == self.name:
-                    return row[0]
+        if self.name:
+            with open(CUST_CSV) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=",")
+                for row in csv_reader:
+                    if row[1] == self.name:
+                        return row[0]
         return None
 
     def get_orders_by_id(self, id):
-        with open(ORDERS_CSV) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=",")
-            for row in csv_reader:
-                if row[1] == id:
-                    self.filtered_orders.append(row)
+        if id is not None:
+            with open(ORDERS_CSV) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=",")
+                for row in csv_reader:
+                    if row[1] == id:
+                        self.filtered_orders.append(row)
 
     def calc_sum_of_orders(self):
-        sum=0
+        sum = 0
         for order in self.filtered_orders:
             sum += int(order[3])
 
